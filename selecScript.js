e137c26571c9ab3374f91f086b4cc814763583a7
kihen0 <kirill.morozowhts@gmail.com>
2017-07-14 00:09:58 +0300

    ahhh
    doesn't work

---
+++ selecScript.js
@@ -0,0 +1,10 @@
+function makeOpts() {
+  var i=document.getElementById('numval');
+  var n=i.value;
+  n=Math.sqrt(n*n);
+  document.write("<select");
+  for (var i = 0; i < n; i++) {
+
+  }
+  document.write('<p><input type=&quot;submit&quot; ></input></p>');
+}
